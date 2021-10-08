ddb2dde217c20299820ff4904bdda02315332710
ChusaMM <chusamoret@gmail.com>
2021-10-08 09:25:26 +0200

    First

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { NavController } from '@ionic/angular';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -10,7 +11,7 @@ export class LoginPage implements OnInit {
 
   formLogin: FormGroup;
   email ='chusamoret@gmail.com';
-  pass='1234';
+  pass='12345';
   
 
   constructor(public formBuilder: FormBuilder,public navCtrl: NavController ) { }
@@ -20,17 +21,32 @@ export class LoginPage implements OnInit {
   }
   createForm() {
     this.formLogin = this.formBuilder.group({
-      email: [''],
-      password: [''],
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(5)])],
     });
   }
   login(){
-
     if(this.formLogin.controls.email.value == this.email && this.formLogin.controls.password.value == this.pass){
+    
+
+  switch (true) {
+    case !this.formLogin.controls.email.valid && !this.formLogin.controls.password.valid:
+      alert('El email y la contraseña están en blanco o no tienen un formato válido');
+      break;
+    case !this.formLogin.controls.email.valid:
+      alert( 'El email está en blanco o no tiene un formato válido');
+      break;
+    case!this.formLogin.controls.password.valid:
+      alert('La contraseña está en blanco o no tiene un formato válido, debe contener un mínimo de cinco caracteres');
+      break;
+    case this.formLogin.valid:
+      alert('OK!');
       this.navCtrl.navigateForward('home');
-    }
-  
+      break;
+  }
+   
   }
 }
 
+}
 
